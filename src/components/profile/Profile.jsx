e74c0c38b848570a032297369cb92e5cb7c1b6d8
VolZa <vvolza@gmail.com>
2023-04-12 18:24:57 +0300

    Хочу відкотитись до попереньої версії

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,23 +1,26 @@
-import PropTypes from 'prop-types';
-import { Container } from './Profile.styled';
-export const Profile = ({ userName, tag, location, avatar, stats }) => {
-    return (
-        <Container >
-            <div>
-                <img src={avatar} alt={userName} />
-                <p>{userName}</p>
-                <p>@{tag}</p>
-                <p>{location}</p>
-            </div>
-            <ul>
-                <li>Followers <span>{stats.followers}</span> </li>
-                <li>Views <span>{stats.views}</span></li>
-                <li>Likes <span>{stats.likes}</span></li>              
-            </ul>
-        </Container>
+// import PropTypes from 'prop-types';
+// import { Container } from './Profile.styled';
+// export const Profile = ({ userName, tag, location, avatar, stats }) => {
+//     return (
+//         // <Container >
+//         <div>    
+//             <div>
+//                 {/* <img src={avatar} alt={userName} />
+//                 <p>{userName}</p>
+//                 <p>@{tag}</p>
+//                 <p>{location}</p>
+//             </div>
+//             <ul>
+//                 <li>Followers <span>{stats.followers}</span> </li>
+//                 <li>Views <span>{stats.views}</span></li>
+//                 <li>Likes <span>{stats.likes}</span></li>               */}
+//                 Vasia
+//             </ul>
+//         </div>    
+ 
 
-    );
-}
+//     );
+// }
 
  
 
@@ -25,14 +28,14 @@ export const Profile = ({ userName, tag, location, avatar, stats }) => {
 
 
 // Типізація для компонента
-Profile.propTypes = {
-  userName: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-  }).isRequired,
-};
+// Profile.propTypes = {
+//   userName: PropTypes.string.isRequired,
+//   tag: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   avatar: PropTypes.string.isRequired,
+//   stats: PropTypes.shape({
+//     followers: PropTypes.number.isRequired,
+//     likes: PropTypes.number.isRequired,
+//     views: PropTypes.number.isRequired,
+//   }).isRequired,
+// };
