035ce1e3f2bcd1272122b49bdb2b994167991177
VolZa <vvolza@gmail.com>
2023-04-13 11:33:02 +0300

    Continue ater parking

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -5,7 +5,7 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-//   width: 100%;
-//   height: 100%;
+  width: 100%;
+  height: 100%;
   text-align: center;
   `;
